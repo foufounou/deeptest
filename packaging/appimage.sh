2228132ed3d2533d860cc689a11d6d21aa9ef0e2
foufounou <foufounou@gmail.com>
2020-04-06 15:43:14 +0100

    random updates

--- packaging/appimage.sh
+++ packaging/appimage.sh
@@ -2,12 +2,11 @@
 
 _APP_VERSION=0.5
 _APP_NAME=DeepTags
-# _PKG_DIR=$(realpath $(dirname $0))
-# _ROOT=$(realpath $_PKG_DIR/..)
 _PKG_DIR=$(readlink -f $(dirname $0))
 _ROOT=$(readlink -f $_PKG_DIR/..)
 _DESKTOP_FILE=$_ROOT/$_APP_NAME.desktop
 _ICON_FILE=$_ROOT/$_APP_NAME.png
+_ICON_DIR=$_ROOT/images/icons
 _TRANSLATIONS=$_ROOT/locale/*.qm
 _APP_BIN=$_ROOT/build/release/$_APP_NAME
 _APPIMAGE_DIR=$_PKG_DIR/deeptags_"$_APP_VERSION"
@@ -18,33 +17,50 @@ rm -rf $_APPIMAGE_DIR
 
 # Check if we are on Linux
 if [ "$(uname)" != "Linux" ]; then
-    echo "This script only works on linux."
+    >&2 echo "This script only works on linux."
     exit 1
 fi
 
 if [ ! -f "$_APP_BIN" ]; then
-    echo "the binary of $_APP_NAME doesn't exist."
+    >&2 echo "the binary of $_APP_NAME doesn't exist."
     exit 1
 fi
 
+# download linuxdeployqt if it doesn't exist
+if [ ! -f $_LINUXDEPLOYQT ]; then
+    >&2 echo "LinuxDeployQt doesn't exit."
+    echo "Downloading LinuxDeployQt..."
+    wget -P $_PKG_DIR -c https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
+    chmod +x $_LINUXDEPLOYQT
+fi
+
+echo "creating the directory structure"
 mkdir -p $_APPIMAGE_DIR/usr/bin
-mkdir -p $_APPIMAGE_DIR/usr/lib
 mkdir -p $_APPIMAGE_DIR/usr/translations
 mkdir -p $_APPIMAGE_DIR/usr/share/applications
-mkdir -p $_APPIMAGE_DIR/usr/share/icons/hicolor/256x256/apps/
 
 echo "copying necessary files"
-cp $_DESKTOP_FILE $_APPIMAGE_DIR/usr/share/applications
-cp $_ICON_FILE $_APPIMAGE_DIR/usr/share/icons/hicolor/256x256/apps/
-cp $_DESKTOP_FILE $_APPIMAGE_DIR/
-cp $_ICON_FILE $_APPIMAGE_DIR/
-cp $_TRANSLATIONS $_APPIMAGE_DIR/usr/translations/
-cp $_APP_BIN $_APPIMAGE_DIR/usr/bin
-cp /usr/lib/x86_64-linux-gnu/libstdc++.so* mkdir -p $_APPIMAGE_DIR/usr/lib/
+cp $_DESKTOP_FILE   $_APPIMAGE_DIR/
+cp $_DESKTOP_FILE   $_APPIMAGE_DIR/usr/share/applications
+cp -r $_ICON_DIR    $_APPIMAGE_DIR/usr/share/
+cp $_ICON_FILE      $_APPIMAGE_DIR/
+cp $_TRANSLATIONS   $_APPIMAGE_DIR/usr/translations/
+cp $_APP_BIN        $_APPIMAGE_DIR/usr/bin
 
-echo "creaeting the appimage"
-$_LINUXDEPLOYQT $_APPIMAGE_DIR/usr/bin/$_APP_NAME -appimage -bundle-non-qt-libs
 
+# if --add-libstdc++ is provided, bundle libstdc++.so.x 
+if [ "$*" == "--add-libstdc++" ]; then
+    echo "copying libstdc++"
+    mkdir -p $_APPIMAGE_DIR/usr/lib
+    cp /usr/lib/x86_64-linux-gnu/libstdc++.so* $_APPIMAGE_DIR/usr/lib/
+fi
+
+echo "creating the appimage"
+RESULT=$($_LINUXDEPLOYQT $_APPIMAGE_DIR/usr/bin/$_APP_NAME -appimage -bundle-non-qt-libs)
+
+if [ $? != 0 ]; then
+    >&2 echo "ERROR. LinuxDeployQt failed."
+fi
 
 echo "deleting temporary files"
 rm -rf $_APPIMAGE_DIR
